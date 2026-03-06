535794fc330a67ebf3780ae278c2a4b822d6ea29
Osada Krzysztof <krzysztof@osada.io>
2026-03-06 07:28:04 +0100

    kro-ttfe: patterns: published on

---
+++ wp-content/plugins/kro-twentytwentyfive-extender/patterns/published-on.php
@@ -0,0 +1,8 @@
+<!-- wp:group {"style":{"spacing":{"blockGap":"0.2em"}},"textColor":"accent-4","fontSize":"small","layout":{"type":"flex","flexWrap":"wrap"}} -->
+<div class="wp-block-group has-accent-4-color has-text-color has-small-font-size" style="margin-top: 0">
+	<!-- wp:paragraph -->
+	<p><?= __( 'Published on ', 'kro-ttfe' ); ?></p>
+	<!-- /wp:paragraph -->
+	<!-- wp:post-date /-->
+</div>
+<!-- /wp:group -->
