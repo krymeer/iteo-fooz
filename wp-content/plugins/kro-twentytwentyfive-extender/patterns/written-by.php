757bcb15895228dfc3e2268d05ad1b6e499e9018
Osada Krzysztof <krzysztof@osada.io>
2026-03-06 07:28:21 +0100

    kro-ttfe: patterns: written by

---
+++ wp-content/plugins/kro-twentytwentyfive-extender/patterns/written-by.php
@@ -0,0 +1,12 @@
+<!-- wp:group {"style":{"spacing":{"blockGap":"0.2em"}},"textColor":"accent-4","fontSize":"small","layout":{"type":"flex","flexWrap":"wrap"}} -->
+<div class="wp-block-group has-accent-4-color has-text-color has-link-color has-small-font-size">
+	<!-- wp:paragraph -->
+	<p><?php esc_html_e( 'Written by ', 'twentytwentyfive' ); ?></p>
+	<!-- /wp:paragraph -->
+	<!-- wp:post-author-name {"isLink":true} /-->
+	<!-- wp:paragraph -->
+	<p><?php esc_html_e( 'in', 'twentytwentyfive' ); ?></p>
+	<!-- /wp:paragraph -->
+	<!-- wp:post-terms {"term":"book-genre","style":{"typography":{"fontWeight":"300"}}} /-->
+</div>
+<!-- /wp:group -->
